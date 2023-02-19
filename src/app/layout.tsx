65ea75c205ff1f2cfd5a2bb6df5f6ee28fb2a863
Muhammad-Tahir <mtz5prif@gmail.com>
2023-02-19 09:12:37 +0100

    refactored gsap animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 "use client";
 import "./globals.css";
-import React, { ReactNode, useLayoutEffect, useRef } from "react";
+import React, { ReactNode, useRef } from "react";
 import LayoutHeader from "@/components/Header";
-import { gsap } from "gsap";
 import Logo from "@/assets/icons/load-logo.svg";
 import tw from "tailwind-styled-components";
+import { useHeaderAnimations } from "@/gsap/header";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const bodyRef = useRef<HTMLBodyElement>(null);
@@ -13,86 +13,23 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     React.useLayoutEffect = React.useEffect;
   }
 
-  useLayoutEffect(() => {
-    const tl = gsap.timeline({});
+  useHeaderAnimations(bodyRef);
 
-    let ctx = gsap.context(() => {
-      tl.from(".center-load-logo", {
-        display: "block",
-        opacity: 0,
-        ease: "expo",
-        duration: 1.5,
-      });
-
-      tl.from(
-        ".nav-logo",
-        {
-          duration: 1.5,
-          opacity: 0,
-          x: window.innerWidth / 2,
-          y: window.innerHeight / 2,
-          xPercent: -50,
-          yPercent: -50,
-          stagger: 0.2,
-          ease: "expo",
-          force3D: true,
-        },
-        1.5
-      );
-
-      tl.from(
-        ".nav-item",
-        {
-          duration: 2,
-          opacity: 0,
-          y: -30,
-          stagger: 0.2,
-          ease: "elastic",
-          force3D: true,
-        },
-        2.5
-      );
-
-      tl.from(
-        ".nav-resume-btn",
-        {
-          duration: 2,
-          opacity: 0,
-          x: 40,
-          stagger: 0.2,
-          ease: "elastic",
-          force3D: true,
-        },
-        2.5
-      );
-
-      tl.from(
-        ".main",
-        {
-          duration: 2,
-          opacity: 0,
-          x: 40,
-          stagger: 0.2,
-          ease: "elastic",
-          force3D: true,
-        },
-        3
-      );
-    }, bodyRef);
-
-    return () => ctx.revert(); // cleanup
-  }, []);
   return (
     <html lang="en">
       <head />
-      <body ref={bodyRef} className="bg-primary-150 overflow-y-scroll">
+      <Body ref={bodyRef}>
         <LayoutHeader />
         <LogoSvg />
-        <div className="main px-[150px] bg-inherit">{children}</div>
-      </body>
+        <ChildrenCon>{children}</ChildrenCon>
+      </Body>
     </html>
   );
 }
+
+const Body = tw.body`
+bg-primary-150 overflow-y-scroll
+`;
 
 const LogoSvg = tw(Logo)`
 hidden absolute left-[calc(50%_-_21px)] 
@@ -100,3 +37,7 @@ top-[calc(50%_-_21px)] center-load-logo
 stroke-primary-700 fill-transparent 
 hover:fill-primary-200 ease-in 
 transition-colors duration-150`;
+
+const ChildrenCon = tw.div`
+main px-10 min-[320px]:px-[150px] bg-inherit
+`;
