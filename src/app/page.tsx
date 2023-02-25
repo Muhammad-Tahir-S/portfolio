69b014eab50eab978b77e44eb63295bdd954100c
Muhammad-Tahir <mtz5prif@gmail.com>
2023-02-25 19:18:18 +0100

    added intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
-// import { Inter } from '@next/font/google';
 import About from "@/components/About";
 import tw from "tailwind-styled-components";
-
-// const inter = Inter({ subsets: ['latin'] });
+import Text from "@/components/Text";
 
 const Section = tw.section`
-h-[100vh] min-h-[100vh] items-start flex flex-col justify-center
+h-[100vh] min-h-[100vh] items-start 
+flex flex-col justify-center
 `;
 
 export default function Home() {
   return (
     <>
+      <Section>
+        <div className="flex flex-col">
+          <Text variant="L2" color="sec-100">
+            Muhammad-Tahir Sanuth.
+          </Text>
+
+          <Text variant="L1" color="white">
+            React Web Developer.
+          </Text>
+          <Text variant="H5" color="pri-700" className="mt-5">
+            I build user interfaces into highly performant, accessible
+            applications.
+          </Text>
+        </div>
+      </Section>
       <Section id="about">
         <About />
       </Section>
