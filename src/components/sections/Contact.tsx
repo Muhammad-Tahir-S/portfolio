69d08a1b947825ca64f561dc5f87469b05fab475
Muhammad-Tahir <mtz5prif@gmail.com>
2023-07-09 23:09:20 +0100

    add contact links

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -3,6 +3,9 @@ import Github from "@/assets/icons/github.svg";
 import LinkedIn from "@/assets/icons/linked-in.svg";
 
 export default function Contact() {
+  const GITHUB_LINK = "https://github.com/Muhammad-Tahir-S";
+  const LINKEDIN_LINK =
+    "https://www.linkedin.com/in/muhammad-tahir-sanuth-560566104/";
   return (
     <div className="w-full">
       <div className="w-full rounded-sm md:w-[70%] mx-auto text-center bg-gray-300 p-6">
@@ -20,8 +23,12 @@ export default function Contact() {
         </Text>
 
         <div className="flex gap-4 justify-center mt-4 items-start">
-          <Github className="w-8 h-8 fill-primary-250 mt-[2px]" />
-          <LinkedIn className="w-8 h-8 fill-primary-250" />
+          <a href={GITHUB_LINK} target="_blank" rel="noreferrer">
+            <Github className="w-8 h-8 fill-primary-250 mt-[2px]" />
+          </a>
+          <a href={LINKEDIN_LINK} target="_blank" rel="noreferrer">
+            <LinkedIn className="w-8 h-8 fill-primary-250" />
+          </a>
         </div>
       </div>
     </div>
